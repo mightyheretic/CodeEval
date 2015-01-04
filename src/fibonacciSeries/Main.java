e3d74a419a296f1b31ae0eabe2f7449833c0cb8e
mightyheretic <mightyheretic@gmail.com>
2015-01-03 22:54:16 -0500

    Finished Fibonacci Series with single calculation answer

---
+++ src/fibonacciSeries/Main.java
@@ -0,0 +1,16 @@
+package fibonacciSeries;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(new BufferedReader(new FileReader(args[0])));
+		double sfive = Math.sqrt(5.0);
+		while (in.hasNext()) {
+			System.out.println((int) Math.round(Math.pow(((1.0 + sfive) / 2.0),
+					in.nextDouble()) / sfive));
+		}
+	}
+}
